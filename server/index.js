69318db4a9166571d14c8b0ed1eda272d0c0c1d9
George Papagapitos <george.g.papagapitos@gmail.com>
2021-05-17 15:53:47 -0500

    setting up server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import postRouter from './routes/post.router.js';
 
 const app = express();
+
+app.use('/api/v1/posts', postRouter);
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/');
+const dbUrl = process.env.DB_CONNECTION_URL;
+const port = process.env.PORT;
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(port, () => console.log(`Server running on port: ${port}`)))
+  .catch((err) => console.error(err.message));
+
+// mongoose.set('useFindAndModify', false);
